13d3259042ecb0c74abc590f09dc4fb21488da0b
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-16 21:44:02 +0200

    updated

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,3 @@
-// task_1/js/main.ts
 
 interface Teacher {
   firstName: string;
@@ -18,3 +17,33 @@ const teacher1: Teacher = {
 };
 
 console.log(teacher1);
+
+interface Director extends Teacher {
+  numberOfReports: number;
+  fullTimeEmployee: boolean;
+}
+
+const director1: Director = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: true,
+    location: 'London',
+    numberOfReports: 17,
+};
+
+console.log(director1);
+interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+class StudentClass {}
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName?(): string;
+}
