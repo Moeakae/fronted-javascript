2e242870e906ce6677bc59b0eac05f3319384509
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-16 19:54:10 +0200

    updated

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    isEnrolled: boolean;
+}
+
+const student1: Student = {
+    firstName: "Alice",
+    lastName: "Johnson",
+    age: 20,
+    isEnrolled: true,
+};
+
+const student2: Student = {
+    firstName: "Bob",
+    lastName: "Smith",
+    age: 22,
+    isEnrolled: false,
+};
