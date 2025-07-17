96af31e96c25db5da699a0d939a89cf3192347c1
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-17 17:00:07 +0200

    updated

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,25 @@
+// task_4/js/subjects/Java.ts
+/// <reference path="Subject.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    teacher?: Teacher;
+
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
+
